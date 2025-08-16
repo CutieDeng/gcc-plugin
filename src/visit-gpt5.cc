30544cb5a35372a949c1578dc9cc524db65106f0
CutieDeng <CutieDeng@iCloud.com>
2025-08-17 01:48:19 +0800

    (FEAT "gpt5: update codes")
    "version 1.3-0817-0147"
    "compile error: ipa_execute"

--- src/visit-gpt5.cc
+++ src/visit-gpt5.cc
@@ -14,36 +14,18 @@
 #include "vec.h"
 #include "hash-table.h"
 #include "symtab.h"
-#include "tree-pass.h"   // ⚠️ 正确的 pass API 头文件
+#include "tree-pass.h"
 
 int plugin_is_GPL_compatible;
 
 #define DEBUG_PRINT(fmt, ...) \
-    printf("[DEBUG][%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-
-// ===== 错误码定义 =====
-enum AnalyzerErr {
-    ERR_NONE                = 0,
-    ERR_FRAMEWORK_LOGIC     = 1 << 0,
-    ERR_SOFT_ASSERT         = 1 << 1,
-    ERR_LOGIC               = 1 << 2,
-    ERR_MEMORY              = 1 << 3
-};
-
-#define TRY(x) do { long long _e = (x); if (_e) { \
-    DEBUG_PRINT("TRY failed -> code=%lld", _e); \
-    return _e; } } while (0)
-
-#define TRY_WEAK(x) do { long long _e = (x); if (_e && _e != ERR_SOFT_ASSERT) { \
-    DEBUG_PRINT("TRY_WEAK failed -> code=%lld", _e); \
-    return _e; } } while (0)
+    printf("[DEBUG] " fmt "\n", ##__VA_ARGS__)
 
 // ===== 数据存储结构 =====
 struct AccessTuple {
     tree struct_type;
     tree field_decl;
     tree element_type;
-    gimple *member_stmt;
     gimple *element_stmt;
     int rw_mode; // 0 = read, 1 = write
 };
@@ -53,12 +35,9 @@ class Analyzer {
 public:
     auto_vec<AccessTuple> results;
 
-    Analyzer() {}
-    ~Analyzer() {}
-
     long long analyze_function(cgraph_node *node) {
         if (!gimple_has_body_p(node->decl))
-            return ERR_NONE;
+            return 0;
 
         push_cfun(DECL_STRUCT_FUNCTION(node->decl));
         basic_block bb;
@@ -66,67 +45,48 @@ public:
             for (gimple_stmt_iterator gsi = gsi_start_bb(bb);
                  !gsi_end_p(gsi); gsi_next(&gsi)) {
                 gimple *stmt = gsi_stmt(gsi);
-                TRY_WEAK(analyze_stmt(stmt));
+                analyze_stmt(stmt);
             }
         }
         pop_cfun();
-        return ERR_NONE;
+        return 0;
     }
 
-    long long analyze_stmt(gimple *stmt) {
+    void analyze_stmt(gimple *stmt) {
         if (is_gimple_assign(stmt)) {
             tree lhs = gimple_assign_lhs(stmt);
             tree rhs = gimple_assign_rhs1(stmt);
 
-            if (TREE_CODE(lhs) == ARRAY_REF || TREE_CODE(rhs) == ARRAY_REF) {
-                tree arr = (TREE_CODE(lhs) == ARRAY_REF ? lhs : rhs);
-                tree base = TREE_OPERAND(arr, 0);
-
-                if (TREE_CODE(base) == COMPONENT_REF &&
-                    TREE_CODE(TREE_OPERAND(base, 0)) == INDIRECT_REF) {
-                    tree ind = TREE_OPERAND(base, 0);
-                    tree comp = base;
-                    tree structtype = TREE_TYPE(TREE_OPERAND(ind, 0));
-                    tree field = TREE_OPERAND(comp, 1);
-                    tree elemtype = TREE_TYPE(arr);
-
-                    AccessTuple tup;
-                    tup.struct_type = structtype;
-                    tup.field_decl = field;
-                    tup.element_type = elemtype;
-                    tup.member_stmt = stmt;
-                    tup.element_stmt = stmt;
-                    tup.rw_mode = classify_rw(lhs, rhs);
-
-                    results.safe_push(tup);
-                }
+            if (lhs && TREE_CODE(lhs) == ARRAY_REF) {
+                AccessTuple tup {};
+                tup.struct_type = TREE_TYPE(TREE_OPERAND(lhs, 0));
+                tup.element_type = TREE_TYPE(lhs);
+                tup.element_stmt = stmt;
+                tup.rw_mode = 1; // 写
+                results.safe_push(tup);
+            }
+            if (rhs && TREE_CODE(rhs) == ARRAY_REF) {
+                AccessTuple tup {};
+                tup.struct_type = TREE_TYPE(TREE_OPERAND(rhs, 0));
+                tup.element_type = TREE_TYPE(rhs);
+                tup.element_stmt = stmt;
+                tup.rw_mode = 0; // 读
+                results.safe_push(tup);
             }
         }
-        return ERR_NONE;
-    }
-
-    int classify_rw(tree lhs, tree rhs) {
-        if (TREE_CODE(lhs) == ARRAY_REF)
-            return 1;
-        return 0;
     }
 
     void dump_results() {
         for (unsigned i = 0; i < results.length(); ++i) {
             AccessTuple &t = results[i];
             printf("==== Access Tuple ====\n");
-            if (t.struct_type) {
+            if (t.struct_type)
                 printf("StructType: %s\n", get_tree_code_name(TREE_CODE(t.struct_type)));
-            }
-            if (t.field_decl) {
-                printf("Field: %s\n", get_tree_code_name(TREE_CODE(t.field_decl)));
-            }
-            if (t.element_type) {
+            if (t.element_type)
                 printf("ElementType: %s\n", get_tree_code_name(TREE_CODE(t.element_type)));
-            }
             printf("RW Mode: %d\n", t.rw_mode);
             printf("Stmt:\n");
-            print_gimple_stmt(stdout, t.element_stmt, 0, 0);
+            print_gimple_stmt(stdout, t.element_stmt, 0, TDF_NONE);
             printf("\n");
         }
     }
@@ -134,16 +94,14 @@ public:
 
 // ===== IPA Pass =====
 namespace {
-const ipa_opt_pass_data ipa_pass_data = {
-    .type = IPA_PASS,
-    .name = "ptr_access_analyzer",
-    .optinfo_flags = OPTGROUP_NONE,
-    .tv_id = TV_NONE,
-    .properties_required = PROP_gimple_any,
-    .properties_provided = 0,
-    .properties_destroyed = 0,
-    .todo_flags_start = 0,
-    .todo_flags_finish = 0,
+
+const pass_data ipa_pass_data = {
+    IPA_PASS,                      // type
+    "ptr_access_analyzer",         // name
+    OPTGROUP_NONE,                 // optinfo_flags
+    TV_NONE,                       // tv_id
+    PROP_gimple_any,               // properties_required
+    0, 0, 0, 0                     // provided/destroyed/todo_flags
 };
 
 struct ipa_analyzer_pass : simple_ipa_opt_pass {
@@ -152,21 +110,22 @@ struct ipa_analyzer_pass : simple_ipa_opt_pass {
 
     unsigned int execute(function *) override { return 0; }
 
-    void ipa_execute(cgraph_node *) override {
+    void ipa_execute() override {
         Analyzer analyzer;
-        FOR_EACH_DEFINED_FUNCTION(cnode) {
-            if (!gimple_has_body_p(cnode->decl))
-                continue;
-            analyzer.analyze_function(cnode);
+        FOR_EACH_DEFINED_FUNCTION(node) {
+            if (!gimple_has_body_p(node->decl)) continue;
+            analyzer.analyze_function(node);
         }
         analyzer.dump_results();
     }
 };
+
 } // namespace
 
 // ===== 插件入口 =====
 int plugin_init(struct plugin_name_args *plugin_info,
                 struct plugin_gcc_version *version) {
+
     if (!plugin_default_version_check(version, &gcc_version)) {
         printf("Incompatible gcc/plugin versions\n");
         return 1;
@@ -184,6 +143,6 @@ int plugin_init(struct plugin_name_args *plugin_info,
                       nullptr,
                       &pass_info);
 
-    printf("Plugin %s loaded\n", plugin_info->base_name);
+    printf("Plugin %s loaded.\n", plugin_info->base_name);
     return 0;
 }
